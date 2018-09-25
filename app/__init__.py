d2fc57561cf9971d9c8a85cc43e34d4292b5d9e6
ZuoXu <731933255@qq.com>
2018-09-25 17:14:49 +0800

    work on bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy  import  SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
-
+from flask_bootstrap import Bootstrap
 # 扩展应用 必须先初始化
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,6 +12,7 @@ migrate =Migrate(app,db)
 login = LoginManager(app)
 # flask-login 用户访问权限控制 如果没有登录 会强制跳转到指定的 视图 所以需要指定所需要跳转的视图
 login.login_view = 'login'
+bootstrap = Bootstrap(app)
 
 
 
